aac56f5ff350b27e9c59905450eee18d968eb0d4
Idan Garay <47768819+Idan-Garay@users.noreply.github.com>
2021-10-15 21:18:39 +0800

    generate webpack template

---
+++ src/components/Day.js
@@ -0,0 +1,10 @@
+import "./Day.css";
+
+export default function Day() {
+  return (
+    <div className="day">
+      <h1>Highlight of the day</h1>
+      <p>12 Tasks </p> Hello World
+    </div>
+  );
+}
