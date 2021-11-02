7347e361ec1ad4d2bc11eab41ec5856c00d7345f
Idan Garay <47768819+Idan-Garay@users.noreply.github.com>
2021-11-02 09:13:07 +0800

    updated upstream

--- src/index.js
+++ src/index.js
@@ -2,22 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import "./index.css";
-<<<<<<< Updated upstream
 import { BrowserRouter as Router } from "react-router-dom";
-=======
-import { Router, Route } from "react-router-dom";
->>>>>>> Stashed changes
 import { createBrowserHistory } from "history";
 
 const history = createBrowserHistory();
 
 ReactDOM.render(
   <Router history={history}>
-<<<<<<< Updated upstream
-    <App />
-=======
     <Route path="/" component={App} />
->>>>>>> Stashed changes
   </Router>,
   document.getElementById("root")
 );
