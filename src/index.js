3145df4e435a55e4e09a0583838cba80bdb1d032
Idan Garay <47768819+Idan-Garay@users.noreply.github.com>
2021-11-16 07:09:27 +0800

    delete model dir

--- src/index.js
+++ src/index.js
@@ -3,9 +3,6 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import "./index.css";
 import { BrowserRouter as Router } from "react-router-dom";
-import { createBrowserHistory } from "history";
-
-// const history = createBrowserHistory();
 
 ReactDOM.render(
   <Router>
