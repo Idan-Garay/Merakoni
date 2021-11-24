60d8d021d9d1e75991a76b424148563e1db0d843
Idan Garay <47768819+Idan-Garay@users.noreply.github.com>
2021-11-24 19:02:19 +0800

    fixed badges key error

--- src/components/Badges/Unacquired.js
+++ src/components/Badges/Unacquired.js
@@ -1,21 +1,17 @@
-import React from 'react';
+import React from "react";
 
-const Unacquired = ({point, array}) => {
-    const badges = array.map(badge =>{
-        if(point < badge.point){
-            return (
-                <div className="badges">
-                    <img src={require(`./Images/${badge.src}.png`)} alt=""/>
-                    <p>{badge.title}</p>
-                </div>
-            )
-        }
-    })
-    return (
-        <div className="badgeList">
-            {badges}
+const Unacquired = ({ point, array }) => {
+  const badges = array.map((badge, index) => {
+    if (point < badge.point) {
+      return (
+        <div className="badges" key={index}>
+          <img src={require(`./Images/${badge.src}.png`)} alt="" />
+          <p>{badge.title}</p>
         </div>
-    );
-}
+      );
+    }
+  });
+  return <div className="badgeList">{badges}</div>;
+};
 
 export default Unacquired;
